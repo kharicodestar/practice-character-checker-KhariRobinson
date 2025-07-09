3046ec9830cd9536205b02e8c16f5aa8f8158201
Khari <kharicodestar@gmail.com>
2025-07-09 17:32:49 -0500

    Updated Answers

---
+++ prac.js
@@ -0,0 +1,11 @@
+const readline = require('readline-sync')
+
+let jsCreator = "Johnny Walker";
+
+let firstInitial = jsCreator[0];
+
+let lastInitial = jsCreator[7];
+
+let outputStr = "JavaScript was created by somebody with the initials " + firstInitial + "." +      lastInitial + ".";
+
+console.log(outputStr);
